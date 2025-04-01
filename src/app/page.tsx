28df21dac6ec648cae1f0dd2f486e6cb0c13f27f
aifuun <aifeng.wu@icloud.com>
2025-04-01 09:03:01 +0900

    signUp ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Auth from "./auth/page";
 
 export default function Home() {
   return (
     <div>
-      <Auth />
+      <a href="/auth">Auth</a>
+
+      <h1>Hello world!</h1>
+      <div>
+        <h2>Sign UP:</h2>
+        <a href="/signUp">Sign Up</a>
+        
+      </div>
+        
     </div>
   );
 }
